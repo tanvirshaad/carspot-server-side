dd580d073095f53c07b9707e3a9b386dcb31fb5d
tanvirshaad <tanvirshaad@gmail.com>
2021-11-12 19:16:33 +0600

    order post and get API added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7o9so.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -23,11 +23,12 @@ async function run() {
         const database = client.db('car_shop');
         const productsCollection = database.collection('products');
         const orderCollection = database.collection('orders');
+        const usersCollection = database.collection('users');
         //GET products API
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({});
             const products = await cursor.toArray();
-            res.send(products);
+            res.json(products);
         });
         //GET single product API
         app.get('/products/:id', async (req, res) => {
@@ -40,6 +41,19 @@ async function run() {
         app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
+            res.json(result);
+        });
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.json(orders);
+        });
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
             res.json(result);
         });
     } finally {
