db9f476e37e49a61af1146e1197c918ca6360539
tanvirshaad <tanvirshaad@gmail.com>
2021-11-13 23:06:07 +0600

    product delete api added

--- index.js
+++ index.js
@@ -89,7 +89,13 @@ async function run() {
             const reviews = await cursor.toArray();
             res.json(reviews);
         });
-        //Delete order APi
+        //Delete APi
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.json(result);
+        });
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
